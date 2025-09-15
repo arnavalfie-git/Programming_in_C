a7e835565312a1b4501922f59dd4319ba028019f
Arnav Singh <arnav.alfie@gmail.com>
2025-09-15 23:46:38 +0530

    Update Experiment_3.1.c

--- Experiment-3/Experiment_3.1.c
+++ Experiment-3/Experiment_3.1.c
@@ -1,1 +1,20 @@
+/*Experiment 3: Conditional Statements
 
+3.1.Write a C program to check whether a number is Even or ODD*/
+
+#include <stdio.h>
+
+int main(){
+	
+	int a;
+	printf("Enter the number you wish to check whether it is odd or even.");
+	scanf("%d",&a);
+	
+	if(a%2 == 0){
+		printf("The entered number is even.");
+	}
+	else{
+		printf("The entered number is odd.");
+	}
+	return 0;
+}
