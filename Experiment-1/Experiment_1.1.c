07f367395955fac26173c7f45cadb1c0f3a299d2
arnavalfie-git <arnav.alfie@gmail.com>
2025-09-15 14:17:02 +0530

    Update Experiment_1.1.c

--- Experiment-1/Experiment_1.1.c
+++ Experiment-1/Experiment_1.1.c
@@ -1,1 +1,10 @@
-
+/*Experiment 1: Installation, Environment Setup, and starting with the C language*/
+/*1. Write a C program to print “ Hello World”*/
+#include <stdio.h>
+int main(){
+	
+	printf("Name - Arnav Singh\nSAP ID - 590027588\nCourse - B.Sc\n");
+	printf("Hello World");
+	
+	return 0;	
+}
