6c1ab61cb268857cdbbccbf1746e8982109dd998
arnavalfie-git <arnav.alfie@gmail.com>
2025-09-15 14:19:15 +0530

    Create Experiment_1.2.c

---
+++ Experiment-1/Experiment_1.2.c
@@ -0,0 +1,9 @@
+/*2. Write a C Program to print the address in multiple lines (new line).*/
+#include <stdio.h>
+
+int main(){
+	
+	printf("Your Space Boys Hostel,\nKandoli,\nNear UPES");
+
+	return 0;
+}
