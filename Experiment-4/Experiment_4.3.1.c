acd73012f7823c9deb880415e35f58dbe66e90db
Arnav Singh <arnav.alfie@gmail.com>
2025-10-14 16:06:43 +0530

    Create Experiment_4.3.1.c

---
+++ Experiment-4/Experiment_4.3.1.c
@@ -0,0 +1,19 @@
+/* 4.3.1 */
+
+#include<stdio.h>
+int main(){
+	printf("Name: Arnav Singh\nSAP ID: 590027588\nCourse: BSc CS\nBatch: B1");
+	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+	int i,j;
+	
+	for(i=1;i<6;i++){
+		
+		for(j=1;j<=i;j++){
+			printf("%d",i);
+		}
+	printf("\n");
+	}
+
+	return 0;
+}
