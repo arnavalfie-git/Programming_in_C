bb059d6d2a5b8688a2ae050682f1e86975837386
Arnav Singh <arnav.alfie@gmail.com>
2025-11-03 11:55:21 +0530

    Add pattern printing program in Day_24.1.c
    This program prints a pattern of asterisks in increasing order.

---
+++ 100_Days_Code/Day-24/Day_24.1.c
@@ -0,0 +1,24 @@
+/* Q47: Write a program to print the following pattern:
+*
+**
+***
+****
+***** */
+
+#include<stdio.h>
+
+int main(){
+	printf("Name: Arnav Singh\nSAP ID: 590027588\nCourse: BSc CS\nBatch: B1");
+	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	
+	int i, j;
+	
+	for(i = 1; i <= 5; i++){
+		for(j = 1; j <= i; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
