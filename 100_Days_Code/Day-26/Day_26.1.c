b2b68cb44783773a3c642fc6b7099740f804cf92
Arnav Singh <arnav.alfie@gmail.com>
2025-11-04 01:29:17 +0530

    Add pattern printing program in Day_26.1.c
    This program prints a specific number pattern in decreasing order.

---
+++ 100_Days_Code/Day-26/Day_26.1.c
@@ -0,0 +1,27 @@
+/* Q51: Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345 */
+
+#include<stdio.h>
+
+int main(){
+	printf("Name: Arnav Singh\nSAP ID: 590027588\nCourse: BSc CS\nBatch: B1");
+	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	
+	int i, j, k;
+	
+	for(i = 5; i >= 1; i--){
+		for(k = 1; k < i; k++){
+			printf(" ");
+		}
+		for(j = i; j <= 5; j++){
+			printf("%d", j);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
