98b9d1343d48d0467206cee964caae196eb780e9
Arnav Singh <arnav.alfie@gmail.com>
2025-11-23 22:29:33 +0530

    Create Day-86.c

---
+++ 100_Days_Code/Day-86.c
@@ -0,0 +1,29 @@
+/* Q136: Use enum to represent menu choices (ADD, SUBTRACT, MULTIPLY) and perform operations using switch. */
+
+#include <stdio.h>
+
+int main() {
+    printf("Name: Arnav Singh\nSAP ID: 590027588\nCourse: BSc CS\nBatch: B1");
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+    enum menu {ADD = 1, SUBTRACT, MULTIPLY};
+
+    int choice, a, b;
+    scanf("%d %d %d", &choice, &a, &b);
+
+    switch(choice) {
+        case ADD:
+            printf("%d\n", a + b);
+            break;
+        case SUBTRACT:
+            printf("%d\n", a - b);
+            break;
+        case MULTIPLY:
+            printf("%d\n", a * b);
+            break;
+        default:
+            printf("Invalid choice\n");
+    }
+
+    return 0;
+}
